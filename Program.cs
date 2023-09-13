4b33cd480b8968536eb87cecbc1942f40ebf73c5
Canialon <canialon666@gmail.com>
2023-09-13 18:57:19 +0100

    add some fixes

--- Program.cs
+++ Program.cs
@@ -13,8 +13,8 @@ if (!app.Environment.IsDevelopment())
     app.UseHsts();
 }
 
-app.UseHttpsRedirection();
-app.UseStaticFiles();
+app.UseHttpsRedirection(); //for pereadresations
+app.UseStaticFiles(); //  in wwwroot
 
 app.UseRouting();
 
