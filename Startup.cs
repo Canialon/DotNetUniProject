cc974ef50f852429ecb53f09da84607ec874527c
Canialon <canialon666@gmail.com>
2023-09-13 18:48:44 +0100

    add missed Startap.cs file

---
+++ Startup.cs
@@ -0,0 +1,46 @@
+namespace YourNamespace
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllersWithViews();
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                endpoints.MapControllerRoute(
+                    name: "random",
+                    pattern: "random",
+                    defaults: new { controller = "RandomNumber", action = "Index" }); // Добавьте это правило
+            });
+        }
+    }
+}
